d9ad2bbcc6aa9eb6204268afd6c96e5faa364de0
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-12-02 13:28:42 +0100

    pay attention

--- python/day_01_1.py
+++ python/day_01_1.py
@@ -1,20 +1,13 @@
-DATA_FILE = "../data/day_01_1.txt"
-
-
-def read_values(fin):
-    with open(fin) as f:
-        return [int(v) for v in f.readlines() if v]
-
-
-def calc_fuel(weight):
-    return weight // 3 - 2
-
-
 def main():
-    values = read_values(DATA_FILE)
-    fuel = sum([calc_fuel(value) for value in values])
-
-    print(fuel)
+    do { 
+            shut_down_your_computer()
+            leave_home()
+            # enjoy_life tasks
+            fuck()
+            buy_dirty_vegetables()
+            find_clothers_in_trash_containers()
+            sleep()
+    } while (wasting_your_life_with_shit())
 
 
 if __name__ == "__main__":
